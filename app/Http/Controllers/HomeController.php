952abee87c11305c4943cc08cbef0f2781dccb24
yazdan ul haq <yazdanulhaq51214@gmail.com>
2022-11-07 16:18:50 +0500

    google authenticator integrated

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+    public function enableMFA()
+    {
+        $authUser = auth()->user();
+        if(!empty($authUser)){
+            $authUserID    = $authUser->id;
+            $authUserName  = $authUser->name;
+            $authUserEmail = $authUser->email;
+            $secret        = $authUserID.rand(0,200).$authUserEmail;
+            $url           = 'https://www.authenticatorApi.com/pair.aspx?AppName=MyApp&AppInfo='.$authUserName.'&SecretCode='.$secret;
+            $qrCode        = $this->httpRequest($url , 'GET');
+            DB::table('users')->where('id',$authUserID)->update([ 'mfa_secret' => $secret ]);
+            return view('mfa-code',[
+                'qrCode'      => $qrCode,
+                'secret'      => $secret,
+                'mfaEnabled'  => $authUser->mfa_enabled,
+            ]);
+
+        } else {
+            abort('401');
+        }
+    }
+    public function verifyMFACode(Request $request)
+    {
+        $authUser = auth()->user();
+        if(!empty($authUser)){
+            $data          = $request->all();
+            $authUserID    = $authUser->id;
+            $authUserEmail = $authUser->email;
+            $mfaEnabled    = $data['mfaEnabled'];
+            $mfaSecret     = $data['secret'];
+            $code          = $data['code'];
+            if($mfaEnabled != $authUser->mfa_enabled) {
+                if($mfaSecret == $authUser->mfa_secret){
+                    $url      = 'https://www.authenticatorApi.com/Validate.aspx?Pin='.$code.'&SecretCode='.$mfaSecret;
+                    $validate = $this->httpRequest($url , 'GET');
+                    if($validate == 'True'){
+                        DB::table('users')->where('id',$authUserID)->update(['mfa_enabled' => $mfaEnabled ]);
+                        $msg = $data['mfaEnabled'] == 'Y' ? 'MFA activated successfully !': 'MFA disabled successfully';
+                        return response()->json([ 'msg' => $msg ,'mfa_enabled' => $mfaEnabled , 'status' => true ],200);
+                    } else {
+                        return response()->json([ 'msg' => 'Invalid code' , 'status' => false ],200);
+                    }
+                  
+                } else {
+                    return response()->json([ 'msg' => 'Invalid code!' , 'status' => false ],200);
+                }
+            }
+        } else {
+            return response()->json([ 'msg' => 'Unauthorized!' , 'status' => false ],401);
+        }
+    }
+    public function httpRequest($url , $type){
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => $type,
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        return $response;
+    }
+}
---
+++ resources/views/mfa-code.blade.php
@@ -0,0 +1,138 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+            <div class="card h-100">
+                <div class="card-content collapse show">
+                    <div class="card-body card-dashboard">
+                        <div class="text-center my-2">
+                        <h2> Two-Factor Authentication</h2>
+                        </div>
+                        <div class="text-center qr-div">
+                            {!! $qrCode !!}
+                        </div>
+                        <div class="text-center">
+                            <button type="button"  class="btn btn-secondary toggle-qr-btn">Hide QR</button>
+                            <fieldset class="mt-2">
+                                <input type="checkbox" class="chk-mfa">
+                                <label for="remember-me"> MFA Enabled </label>
+                            </fieldset>
+                            </div>
+                                <form class="d-flex justify-content-center mt-2 text-center">
+                                    <div class="col-md-3 me-2">
+                                        <div class="form-group">
+                                            <input type="password" class="form-control code" name="code" placeholder="Enter Google Authenticator Code" >
+                                            <input type="hidden" class="form-control secret" name="secret" value="{{ $secret }}">
+                                        </div>
+                                    </div>
+                                    <button type="button" autofocus class="btn btn-secondary verify-code-btn">Verify Code</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('script')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.6/dist/loadingoverlay.min.js"></script>
+<script>
+   
+    $(document).ready(function(){
+        $.LoadingOverlay("show");
+        setTimeout(function(){ 
+            let QRDivContent = $('.qr-div a').html();
+            if(QRDivContent != ""){
+                $('.qr-div').html(QRDivContent);
+            }
+        }, 1000);
+
+        $("[name='code']").keypress(function (e) {
+            var key = e.which;
+            if(key == 13) 
+            {
+                e.preventDefault();
+                $('.verify-code-btn').click();
+            }
+        }); 
+
+       setTimeout(function(){$.LoadingOverlay("hide");},2000);
+       let mfaEnabled = "{{ $mfaEnabled }}";
+       console.log('mfaEnabled : ' , mfaEnabled);
+       if(mfaEnabled == 'Y'){
+           $('.chk-mfa').prop('checked', true)
+       } else {
+          $('.chk-mfa').prop('checked', false)
+       }
+    });
+
+    $('.code').keyup(function(e) {
+        if (/\D/g.test(this.value)){
+            this.value = this.value.replace(/\D/g, '');
+        }
+    });
+
+    $(document).on('click','.toggle-qr-btn',function(){
+        let element = $('.qr-div');
+        let classExists = element.hasClass('d-none');
+        element.toggleClass('d-none');
+        if(classExists){
+            $(this).text('Hide QR');
+        } else {
+            $(this).text('Show QR');
+        }
+    });
+
+    $(document).on('click','.verify-code-btn',function(e){
+        e.preventDefault();
+        let codeSelector = $('.code');
+        let code = codeSelector.val();
+        let secret = $('.secret').val();
+        let mfaEnabled = $('.chk-mfa').is(':checked');
+        mfaEnabled = (mfaEnabled == 0 ? 'N': 'Y');
+        if(codeSelector.val() == "" || code.length != 6){
+            alert("Please enter 6 digits code");
+            codeSelector.addClass('border-red');
+        } else {
+            codeSelector.removeClass('border-red');
+            $.LoadingOverlay("show");
+            $.ajax({
+                url: "{{ route('verifyMFACode')}}",
+                type: 'POST',
+                data: {
+                    'code': code,
+                    'secret': secret,
+                    'mfaEnabled': mfaEnabled,
+                    '_token' : "{{ csrf_token() }}"
+                },
+                headers: {
+                    'Accept' : 'application/json',
+                    'Content-Type' : 'application/x-www-form-urlencoded'
+                },
+                success: function(data){
+                if(data.status){
+                    mfaEnabled  = data.mfa_enabled;
+                    let checked = (mfaEnabled == 'N' ? false: true);  
+                    $('.chk-mfa').attr('checked',checked);
+                    codeSelector.val('');
+                    alert(data.msg);
+
+                } else {
+                    alert(data.msg);
+                }
+                $.LoadingOverlay("hide");
+                },
+                error : function(data){
+                    $.LoadingOverlay("hide");
+                    alert('Whoops, looks like something went wrong');
+                }
+            });
+        }
+    });
+</script>
+@section('script')
